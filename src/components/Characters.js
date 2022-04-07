b80bb82326aa6cae0e0586cf4ea232d64ca74125
Leonardo Puebla <leonardo38361@gmail.com>
2022-04-06 21:51:46 -0300

    Ajustes de css

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -9,6 +9,11 @@ const Characters = ({ characters = [] }) => {
                     <div className='container-img'>
                         <img src={item.image} alt='' />
                     </div>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{item.name}</h5>
+                        <p>specie: {item.species}</p>
+                        <p>location: {item.location.name}</p>
+                    </div>
                 </div>
             ))}
         </div>
